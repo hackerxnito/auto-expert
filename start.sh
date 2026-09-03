be54c6195918ca59b6c46aff5d4ca8be05b74bac
hackerxnito <kennedyngatia18@gmail.com>
2026-09-03 10:07:57 +0300

    Fix Nginx configuration to resolve 403 Forbidden error

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Bind Nginx to Render's dynamic port
-sed -i "s/listen 80;/listen ${PORT:-10000};/g" /etc/nginx/sites-available/default
+sed -i "s/listen 10000;/listen ${PORT:-10000};/g" /etc/nginx/sites-available/default
 
-# Cache Laravel configurations for production performance
+# Cache Laravel optimizations
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Start Nginx and PHP-FPM
+# Start services
 nginx
 php-fpm
